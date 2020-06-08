1c9798e3468c08bc9c303e4b1d4f5b03c93cce38
Ayush <ayushmanojgupta24@gmail.com>
2020-06-08 22:25:17 +0530

    Checking user wins or losses

--- Gambler.sh
+++ Gambler.sh
@@ -2,5 +2,28 @@
 echo "---------Welcome To Gambling---------"
 #variable
 stake=100
+cash=$stake
+winCount=0
+lossCount=0
 #constant
 BET=1
+WIN=1
+
+
+	function Gambler()
+	{
+		BET++
+		winOrLoss=$((RANDOM%2))
+		if (($winOrLoss == $WIN ))
+		then
+			echo "WIN"
+			((cash++))
+		else
+			echo "LOSE"
+			((cash--))
+		fi
+		echo $cash
+		echo $BET
+	}
+
+	echo "$(Gambler)"
