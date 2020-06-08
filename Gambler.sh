fe9843616dbdf66e8a4b1c5fe2d7c807afd5d6a4
Ayush <ayushmanojgupta24@gmail.com>
2020-06-08 22:29:42 +0530

    Cannnot exceed 50 percent of win or loss of stake

--- Gambler.sh
+++ Gambler.sh
@@ -1,29 +1,25 @@
 #!/bin/bash -x
 echo "---------Welcome To Gambling---------"
-#variable
-stake=100
-cash=$stake
-winCount=0
-lossCount=0
-#constant
+#Constants
+STAKE=100
 BET=1
-WIN=1
+ISWIN=1
 
+#Variable
+cash=$STAKE
+minimum=$(($STAKE-$STAKE/2))
+maximum=$(($STAKE+$STAKE/2))
 
-	function Gambler()
-	{
-		BET++
-		winOrLoss=$((RANDOM%2))
-		if (($winOrLoss == $WIN ))
-		then
-			echo "WIN"
-			((cash++))
-		else
-			echo "LOSE"
-			((cash--))
-		fi
-		echo $cash
-		echo $BET
-	}
-
-	echo "$(Gambler)"
+while (( $cash != $minimum && $cash != $maximum ))
+do
+#checking for a win or a loss
+	randomCheck=$((RANDOM%2))
+	if [ $randomCheck -eq $ISWIN ]
+	then
+		echo win
+		((cash++))
+	else
+		echo lose
+		((cash--))
+	fi
+done
